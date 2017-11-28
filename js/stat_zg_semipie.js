0413671a25644989e07993d6165079bd8af77506
Res Marti <res.marti@zg.ch>
2017-11-28 10:59:30 +0100

    Add datatable to semipie and objectified local variables

--- js/stat_zg_semipie.js
+++ js/stat_zg_semipie.js
@@ -1,4 +1,4 @@
-/* stat_zg_pie.js (version 0.2 (2017.07.10)*/
+/* stat_zg_semipie.js (version 0.3 (2017.11.27)*/
 
 function getLegendWidth(number) {
 	var legendLength = d3.select("#chart"+number+" g.dc-legend").node().childElementCount;
@@ -16,15 +16,17 @@ order = typeof order !== 'undefined' ? order : "alpha";
 last = typeof last !== 'undefined' ? last : "";
 partei = typeof partei !== 'undefined' ? partei : false;
 
-var maincontainer="default"+number
-var chartcontainer="chart"+number
+Atts[number]={};
+
+Atts[number].maincontainer="default"+number
+Atts[number].chartcontainer="chart"+number
 
 //Breite des Containers ermitteln
-var totalWidth = document.getElementById(maincontainer).offsetWidth;
+var totalWidth = document.getElementById(Atts[number].maincontainer).offsetWidth;
 totalHeight = 300;
 
 //Charttyp dem Container zuweisen
-Charts[number] = dc.pieChart("#"+chartcontainer);
+Charts[number] = dc.pieChart("#"+Atts[number].chartcontainer);
 
 //Daten einlesen
 var daten = d3.csv(csv_path, function (error, data) {
@@ -32,37 +34,41 @@ var daten = d3.csv(csv_path, function (error, data) {
 		d[group] = +d[group];
 	});
 
-meta = data.filter(function(el) {
+Atts[number].meta = data.filter(function(el) {
 	return el["Meta"] == 1
 });
 	
-data = data.filter(function(el) {
+Atts[number].data = data.filter(function(el) {
 	return el["Meta"] == "NA" | el["Meta"] == undefined
 });
 
-title = meta.filter(function( el ) { return el.Type == "title";});
-if (title.length == 1) {
-$("#"+maincontainer+" #title").html(title[0].Content);
+Atts[number].dataplustotal = data.filter(function(el) {
+	return el["Meta"] == "NA" | el["Meta"] == undefined
+});
+
+Atts[number].title = Atts[number].meta.filter(function( el ) { return el.Type == "title";});
+if (Atts[number].title.length == 1) {
+$("#"+Atts[number].maincontainer+" #title").html(Atts[number].title[0].Content);
 }
 
-subtitle = meta.filter(function( el ) { return el.Type == "subtitle";});
-if (subtitle.length == 1) {
-$("#"+maincontainer+" #subtitle").html(subtitle[0].Content);
+Atts[number].subtitle = Atts[number].meta.filter(function( el ) { return el.Type == "subtitle";});
+if (Atts[number].subtitle.length == 1) {
+$("#"+Atts[number].maincontainer+" #subtitle").html(Atts[number].subtitle[0].Content);
 }
 
-description = meta.filter(function( el ) { return el.Type == "description";});
-if (description.length == 1) {
-$("#"+maincontainer+" #description").html(description[0].Content);
+Atts[number].description = Atts[number].meta.filter(function( el ) { return el.Type == "description";});
+if (Atts[number].description.length == 1) {
+$("#"+Atts[number].maincontainer+" #description").html(Atts[number].description[0].Content);
 }
 
-source = meta.filter(function( el ) { return el.Type == "source";});
-if (source.length == 1) {
-$("#"+maincontainer+" #source").html("Quelle: "+source[0].Content);
+Atts[number].source = Atts[number].meta.filter(function( el ) { return el.Type == "source";});
+if (Atts[number].source.length == 1) {
+$("#"+Atts[number].maincontainer+" #source").html("Quelle: "+Atts[number].source[0].Content);
 }
 
 total=0;
-for (k = 0; k < data.length; k++) {
-	total=total+data[k][group]
+for (k = 0; k < Atts[number].data.length; k++) {
+	total=total+Atts[number].data[k][group]
 };
 
 ZTotal={}
@@ -72,14 +78,14 @@ ZTotal["Meta"] = "NA";
 ZTotal["Type"] = "NA";
 ZTotal["Content"] = "NA";
 
-data.push(ZTotal)
+Atts[number].dataplustotal.push(ZTotal)
 
 //Daten an Crossfilter übergeben
-Datasets[number] = crossfilter(data),
-	MainDimensions[number] = Datasets[number].dimension(function (d) {
+Atts[number].dataset = crossfilter(Atts[number].dataplustotal),
+	Atts[number].maindimension = Atts[number].dataset.dimension(function (d) {
 		return d[dimension];
     }),
-    MainGroups[number] = MainDimensions[number].group().reduceSum(function (d) {
+    Atts[number].maingroup = Atts[number].maindimension.group().reduceSum(function (d) {
         return d[group];
     });
 	
@@ -92,17 +98,17 @@ function arraymove(arr, fromIndex, toIndex) {
 if (typeof characteristics === 'undefined' || characteristics.length==0) {
 	characteristics = [];
 	if (order=="desc"){
-	MainGroups[number].all().sort(function(a,b) {return (a.value > b.value) ? -1 : ((b.value > a.value) ? 1 : 0);} ).forEach(function (x) {
+	Atts[number].maingroup.all().sort(function(a,b) {return (a.value > b.value) ? -1 : ((b.value > a.value) ? 1 : 0);} ).forEach(function (x) {
 			characteristics.push(x["key"]);
 		});
 	}
 	else if (order=="asc"){
-	MainGroups[number].all().sort(function(a,b) {return (a.value > b.value) ? 1 : ((b.value > a.value) ? -1 : 0);} ).forEach(function (x) {
+	Atts[number].maingroup.all().sort(function(a,b) {return (a.value > b.value) ? 1 : ((b.value > a.value) ? -1 : 0);} ).forEach(function (x) {
 			characteristics.push(x["key"]);
 		});
 	}
 	else {
-		MainGroups[number].all().forEach(function (x) {
+		Atts[number].maingroup.all().forEach(function (x) {
 			characteristics.push(x["key"]);
 		});
 	}
@@ -137,8 +143,8 @@ Charts[number].width(totalWidth)
     .cy((totalHeight)-30)
 	.height(totalHeight)
     .slicesCap(15)
-    .dimension(MainDimensions[number])
-    .group(MainGroups[number])
+    .dimension(Atts[number].maindimension)
+    .group(Atts[number].maingroup)
 	.controlsUseVisibility(true)
 	.externalRadiusPadding(-(totalHeight/2)+30)
 	.emptyTitle("Keine Daten vorhanden")
@@ -156,7 +162,7 @@ Charts[number].asc.*/
 	
 Charts[number].filter = function() {};
 
-Counters[number]=0;
+Atts[number].counter=0;
 
 //Legende iniziieren	
 Charts[number].legend(dc.legend().x(10).y(10).itemHeight(13).gap(10)
@@ -176,8 +182,8 @@ Charts[number].on('renderlet', function(chart){
             return translate + ' rotate(90)';
         });
 	getLegendWidth(number);
-	pieWidth = d3.select("#"+chartcontainer+" g:nth-child(1)").node().getBBox().width;	
-	if (Counters[number]==0) {
+	pieWidth = d3.select("#"+Atts[number].chartcontainer+" g:nth-child(1)").node().getBBox().width;	
+	if (Atts[number].counter==0) {
 		if (totalWidth<pieWidth + (2 * legendMaxWidth) + 20) {
 			Charts[number].legend(dc.legend().x(10).y(280).itemHeight(13).gap(10)
 				.horizontal(true)
@@ -191,7 +197,7 @@ Charts[number].on('renderlet', function(chart){
 		};
 		var legendHorizontal = Charts[number].legend().horizontal();
 		if (legendHorizontal==true){
-			var legendHeight = d3.select("#"+chartcontainer+" g.dc-legend").node().getBBox().height;
+			var legendHeight = d3.select("#"+Atts[number].chartcontainer+" g.dc-legend").node().getBBox().height;
 			Charts[number].height(totalHeight+legendHeight-10)
 				.externalRadiusPadding(-(totalHeight/2)+30+(legendHeight/2));
 			Charts[number].redraw();
@@ -201,9 +207,9 @@ Charts[number].on('renderlet', function(chart){
 			Charts[number].redraw();
 		}
 		
-	Counters[number]=1;
-	$("#"+chartcontainer+" g.dc-legend-item:nth-child("+(indexlast+1)+")").hide();
-	$("#"+maincontainer+" .dc-legend-item text").attr("x", 17);
+	Atts[number].counter=1;
+	$("#"+Atts[number].chartcontainer+" g.dc-legend-item:nth-child("+(indexlast+1)+")").hide();
+	$("#"+Atts[number].maincontainer+" .dc-legend-item text").attr("x", 17);
 
 	}
 });
@@ -212,7 +218,7 @@ Charts[number].render()
 
 function initTip(number){
 	last_tip = null;
-	Tips[number] = d3.tip()
+	Atts[number].tips = d3.tip()
 		.attr('class', 'd3-tip')
 		.attr('id', 'd3-tip'+number)
 		.direction('n')
@@ -229,11 +235,11 @@ function initTip(number){
 }
 
 function callTip(number){
-	d3.selectAll("#"+chartcontainer+" g.pie-slice, #"+chartcontainer+" text.pie-slice")
-		.call(Tips[number])
+	d3.selectAll("#"+Atts[number].chartcontainer+" g.pie-slice, #"+Atts[number].chartcontainer+" text.pie-slice")
+		.call(Atts[number].tips)
 		.on('mouseover', function(d, l) {
 			if(d.key !== last_tip) {
-				Tips[number].show(d, $("#"+chartcontainer+" .pie-slice")[characteristics.indexOf(d.data.key)]);
+				Atts[number].tips.show(d, $("#"+Atts[number].chartcontainer+" .pie-slice")[characteristics.indexOf(d.data.key)]);
 				last_tip = d.key;
 			}
 			$(".d3-tip").css("border-left", farben[l]+" solid 5px");
@@ -245,7 +251,7 @@ function callTip(number){
 		})
 		.on('mouseout', function(d) {
 			last_tip = null;
-			Tips[number].hide(d);
+			Atts[number].tips.hide(d);
 		});
 }
 
@@ -255,28 +261,28 @@ callTip(number);
 
 function disableClick(number) {
 	//Click-Event ausschalten
-	d3.selectAll("#"+chartcontainer+" g.pie-slice > path").on('click',null);
-    d3.selectAll("#"+chartcontainer+" g.pie-slice > path").on("click", function() { 
+	d3.selectAll("#"+Atts[number].chartcontainer+" g.pie-slice > path").on('click',null);
+    d3.selectAll("#"+Atts[number].chartcontainer+" g.pie-slice > path").on("click", function() { 
 	});
-	d3.selectAll("#"+chartcontainer+" g.dc-legend-item").on('click',null);
-    d3.selectAll("#"+chartcontainer+" g.dc-legend-item").on("click", function() { 
+	d3.selectAll("#"+Atts[number].chartcontainer+" g.dc-legend-item").on('click',null);
+    d3.selectAll("#"+Atts[number].chartcontainer+" g.dc-legend-item").on("click", function() { 
 	});
 }
 
 disableClick(number);
 
-$("#"+maincontainer+" .dc-legend-item text").attr("x", 17);
-$("#"+chartcontainer+" g.pie-slice-group").attr('transform', 'translate(0 0) rotate(270)');
-$("#"+chartcontainer+" g.pie-label-group").attr('transform', 'translate(0 0) rotate(270)');
+$("#"+Atts[number].maincontainer+" .dc-legend-item text").attr("x", 17);
+$("#"+Atts[number].chartcontainer+" g.pie-slice-group").attr('transform', 'translate(0 0) rotate(270)');
+$("#"+Atts[number].chartcontainer+" g.pie-label-group").attr('transform', 'translate(0 0) rotate(270)');
 var indexlast=characteristicsLength-1
-$("#"+chartcontainer+" g.pie-slice._"+indexlast).hide();
-$("#"+chartcontainer+" text.pie-slice.pie-label._"+indexlast).hide();
-$("#"+chartcontainer+" g.dc-legend-item:nth-child("+(indexlast+1)+")").hide();
+$("#"+Atts[number].chartcontainer+" g.pie-slice._"+indexlast).hide();
+$("#"+Atts[number].chartcontainer+" text.pie-slice.pie-label._"+indexlast).hide();
+$("#"+Atts[number].chartcontainer+" g.dc-legend-item:nth-child("+(indexlast+1)+")").hide();
 
 function resizePie(number) {
 	//Breite des Hauptcontainers einlesen
 	var newWidth = document.getElementById("default"+number).offsetWidth;
-	var pieWidth = d3.select("#"+chartcontainer+" g:nth-child(1)").node().getBBox().width;	
+	var pieWidth = d3.select("#"+Atts[number].chartcontainer+" g:nth-child(1)").node().getBBox().width;	
 	var legendHorizontal = Charts[number].legend().horizontal();
 	Charts[number].width(newWidth)
 		.cx(newWidth/2)
@@ -299,7 +305,7 @@ function resizePie(number) {
 	Charts[number].render()
 	var legendHorizontal = Charts[number].legend().horizontal();
 	if (legendHorizontal==true){
-		var legendHeight = d3.select("#"+chartcontainer+" g.dc-legend").node().getBBox().height;
+		var legendHeight = d3.select("#"+Atts[number].chartcontainer+" g.dc-legend").node().getBBox().height;
 		Charts[number].height(totalHeight+legendHeight-10)
 			.externalRadiusPadding(-(totalHeight/2)+30+(legendHeight/2));
 	}
@@ -314,23 +320,25 @@ function resizePie(number) {
 	
 	callTip(number);
 
-	$("#"+chartcontainer+" g.pie-slice-group").attr('transform', 'translate(0 0) rotate(270)');
-	$("#"+chartcontainer+" g.pie-label-group").attr('transform', 'translate(0 0) rotate(270)');
-	$("#"+chartcontainer+" g.pie-slice._"+indexlast).hide();
-	$("#"+chartcontainer+" text.pie-slice.pie-label._"+indexlast).hide();
-	$("#"+chartcontainer+" g.dc-legend-item:nth-child("+(indexlast+1)+")").hide();
+	$("#"+Atts[number].chartcontainer+" g.pie-slice-group").attr('transform', 'translate(0 0) rotate(270)');
+	$("#"+Atts[number].chartcontainer+" g.pie-label-group").attr('transform', 'translate(0 0) rotate(270)');
+	$("#"+Atts[number].chartcontainer+" g.pie-slice._"+indexlast).hide();
+	$("#"+Atts[number].chartcontainer+" text.pie-slice.pie-label._"+indexlast).hide();
+	$("#"+Atts[number].chartcontainer+" g.dc-legend-item:nth-child("+(indexlast+1)+")").hide();
 	
 	disableClick(number);
 
 	Charts[number].transitionDuration(1500);
-	$("#"+maincontainer+" .dc-legend-item text").attr("x", 17);
+	$("#"+Atts[number].maincontainer+" .dc-legend-item text").attr("x", 17);
 }
 
 $(window).resize(function(){resizePie(number)})
 
 });
 
-addDownloadButton(maincontainer, chartcontainer, number);
-addDownloadButtonPng(maincontainer, chartcontainer, number)
+var columns=[dimension, group]									 
+addDownloadButton(number);
+addDownloadButtonPng(number)
+addDataTablesButton(number, columns)
 
 }
