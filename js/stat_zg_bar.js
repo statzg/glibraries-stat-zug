d428f54106c2e5f7c04349c4fff426c7fc49b135
Res Marti <res.marti@zg.ch>
2017-11-29 15:24:09 +0100

    Clean up default values for bar chart

--- js/stat_zg_bar.js
+++ js/stat_zg_bar.js
@@ -1,20 +1,24 @@
 ﻿/* stat_zg_bar.js (version 0.3 (2017.11.28)*/
 
 function loadBar(args) {
-var number = (args.number== undefined | args.number == "") ? 1 : args.number;
-var csv_path = (args.csv_path== undefined | args.csv_path == "") ? "error" : args.csv_path;
-var dimension = (args.dimension== undefined | args.dimension == "") ? "error" : args.dimension;
-var group = (args.group== undefined | args.group == "") ? "error" : args.group;
-var characteristics = (typeof args.characteristics == 'undefined') ? "error" : args.characteristics;
-var scale = (args.scale== undefined | args.scale == "") ? 1 : args.scale;
-var relative = (args.relative== undefined | args.relative == "") ? false : args.relative;
-var showTotal = (args.showTotal== undefined | args.showTotal == "") ? true : args.showTotal;
-var showAnteil = (args.showAnteil== undefined | args.showAnteil == "") ? true : args.showAnteil;
-var order = (args.order== undefined | args.order == "") ? "alpha" : args.order;
-var last = (args.last== undefined | args.last == "") ? "" : args.last;
-
-//order = typeof order !== 'undefined' ? order : "alpha";
-//last = typeof last !== 'undefined' ? last : "";
+var number = (typeof args.number == 'undefined') ? 1 : args.number;
+var csv_path = (typeof args.csv_path == 'undefined') ? "error" : args.csv_path;
+var dimension = (typeof args.dimension == 'undefined') ? "" : args.dimension;
+var group = (typeof args.group == 'undefined') ? "" : args.group;
+var characteristics = (typeof args.characteristics == 'undefined') ? [] : args.characteristics;
+//var stack = (typeof args.stack == 'undefined') ? "error" : args.stack
+//var characteristicsStack = (typeof args.characteristicsStack == 'undefined') ? "error" : args.characteristicsStack;
+var scale = (typeof args.scale == 'undefined') ? 1 : args.scale;
+//var relative = (typeof args.relative == 'undefined') ? false : args.relative;
+var showTotal = (typeof args.showTotal == 'undefined') ? true : args.showTotal;
+var showAnteil = (typeof args.showAnteil == 'undefined') ? true : args.showAnteil;
+//var showArea = (typeof args.showArea== 'undefined') ? true : args.showArea;
+//var asDate = (typeof args.asDate == 'undefined') ? true : args.asDate;
+//var dateUnit = (typeof args.dateUnit == 'undefined') ? true : args.dateUnit;
+var order = (typeof args.order == 'undefined') ? "alpha" : args.order;
+var last = (typeof args.last == 'undefined') ? "" : args.last;
+//var canton = (typeof args.canton == 'undefined') ? false : args.canton;
+//var partei = (typeof args.partei == 'undefined') ? false : args.partei;
 
 //Attributeobjekt initialisieren
 Atts[number]={};
@@ -31,10 +35,15 @@ Charts[number] = dc.barChart("#"+Atts[number].chartcontainer);
 
 //Daten einlesen
 var daten = d3.csv(csv_path, function(error, data) {
-	data.forEach(function(x) {
-		x[group] = +x[group];
-	});
 
+var dataValues = d3.values(data)[0];
+if (dimension == undefined | dimension=="") {dimension = Object.keys(dataValues)[1];};
+if (group == undefined | group=="") {group = Object.keys(dataValues)[2];};
+	
+data.forEach(function(x) {
+	x[group] = +x[group];
+});
+	
 treatmetadata(number, data);
 
 Atts[number].dataset = crossfilter(Atts[number].data),
