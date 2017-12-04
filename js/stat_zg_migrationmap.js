ea9670c8cc181ce5c2186dd5cb414cd1caa1b71c
Res Marti <res.marti@zg.ch>
2017-12-04 10:37:35 +0100

    Objectified function arguments for migration map

--- js/stat_zg_migrationmap.js
+++ js/stat_zg_migrationmap.js
@@ -1,9 +1,28 @@
-/* stat_zg_migrationmap.js (version 0.3 (2017.11.27)*/
+/* stat_zg_migrationmap.js (version 0.3 (2017.11.28)*/
 
 /*Angepasst von http://bl.ocks.org/cingraham/7663357 */
 
-function loadMigrationMap(number, csv_path) {
+function loadMigrationMap(args) {
+var number = (typeof args.number == 'undefined') ? 1 : args.number;
+var csv_path = (typeof args.csv_path == 'undefined') ? "error" : args.csv_path;
+//var dimension = (typeof args.dimension == 'undefined') ? "" : args.dimension;
+//var group = (typeof args.group == 'undefined') ? "" : args.group;
+//var characteristics = (typeof args.characteristics == 'undefined') ? [] : args.characteristics;
+//var stack = (typeof args.stack == 'undefined') ? "" : args.stack
+//var characteristicsStack = (typeof args.characteristicsStack == 'undefined') ? [] : args.characteristicsStack;
+//var scale = (typeof args.scale == 'undefined') ? 1 : args.scale;
+//var relative = (typeof args.relative == 'undefined') ? false : args.relative;
+//var showTotal = (typeof args.showTotal == 'undefined') ? true : args.showTotal;
+//var showAnteil = (typeof args.showAnteil == 'undefined') ? true : args.showAnteil;
+//var showArea = (typeof args.showArea== 'undefined') ? true : args.showArea;
+//var asDate = (typeof args.asDate == 'undefined') ? true : args.asDate;
+//var dateUnit = (typeof args.dateUnit == 'undefined') ? "month" : args.dateUnit;
+//var order = (typeof args.order == 'undefined') ? "alpha" : args.order;
+//var last = (typeof args.last == 'undefined') ? "" : args.last;
+//var partei = (typeof args.partei == 'undefined') ? false : args.partei;
+//var highlight = (typeof args.highlight == 'undefined') ? {} : args.highlight;
 
+//Attributeobjekt initialisieren
 Atts[number]={};
 
 Atts[number].maincontainer="default"+number
@@ -704,7 +723,7 @@ function simulateClick(elem /* Must be the element, not d3 selection */) {
 $( document ).ready(function() {
 	setTimeout(function() {
 		simulateClick(document.getElementById("ZG"));
-    }, 1000);
+    }, 1500);
 	
 });
 
