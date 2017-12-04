8ba3577840fe70e1abc92002bfaf611549a28aea
Res Marti <res.marti@zg.ch>
2017-12-04 10:49:23 +0100

    Objectified function arguments for pie chart

--- js/stat_zg_pie.js
+++ js/stat_zg_pie.js
@@ -1,4 +1,4 @@
-/* stat_zg_pie.js (version 0.2 (2017.07.10)*/
+/* stat_zg_pie.js (version 0.3 (2017.11.28)*/
 
 function getLegendWidth(number) {
 	var legendLength = d3.select("#chart"+number+" g.dc-legend").node().childElementCount;
@@ -11,10 +11,27 @@ function getLegendWidth(number) {
 	legendMaxWidth = Math.max.apply(null, legendWidthArray);
 }
 
-function loadPie(number, csv_path, dimension, group, characteristics, scale, showTotal, order, last) {
-order = typeof order !== 'undefined' ? order : "alpha";
-last = typeof last !== 'undefined' ? last : "";
+function loadPie(args) {
+var number = (typeof args.number == 'undefined') ? 1 : args.number;
+var csv_path = (typeof args.csv_path == 'undefined') ? "error" : args.csv_path;
+var dimension = (typeof args.dimension == 'undefined') ? "" : args.dimension;
+var group = (typeof args.group == 'undefined') ? "" : args.group;
+var characteristics = (typeof args.characteristics == 'undefined') ? [] : args.characteristics;
+//var stack = (typeof args.stack == 'undefined') ? "" : args.stack
+//var characteristicsStack = (typeof args.characteristicsStack == 'undefined') ? [] : args.characteristicsStack;
+var scale = (typeof args.scale == 'undefined') ? 1 : args.scale;
+//var relative = (typeof args.relative == 'undefined') ? false : args.relative;
+var showTotal = (typeof args.showTotal == 'undefined') ? true : args.showTotal;
+var showAnteil = (typeof args.showAnteil == 'undefined') ? true : args.showAnteil;
+//var showArea = (typeof args.showArea== 'undefined') ? true : args.showArea;
+//var asDate = (typeof args.asDate == 'undefined') ? true : args.asDate;
+//var dateUnit = (typeof args.dateUnit == 'undefined') ? "month" : args.dateUnit;
+var order = (typeof args.order == 'undefined') ? "alpha" : args.order;
+var last = (typeof args.last == 'undefined') ? "" : args.last;
+//var partei = (typeof args.partei == 'undefined') ? false : args.partei;
+//var highlight = (typeof args.highlight == 'undefined') ? {} : args.highlight;
 
+//Attributeobjekt initialisieren
 Atts[number]={};
 
 Atts[number].maincontainer="default"+number;
@@ -29,6 +46,11 @@ Charts[number] = dc.pieChart("#"+Atts[number].chartcontainer);
 
 //Daten einlesen
 var daten = d3.csv(csv_path, function (error, data) {
+	
+var dataValues = d3.values(data)[0];
+if (dimension == undefined | dimension=="") {dimension = Object.keys(dataValues)[0];};
+if (group == undefined | group=="") {group = Object.keys(dataValues)[1];};
+
 	data.forEach(function(d) {
 		d[group] = +d[group];
 	});
