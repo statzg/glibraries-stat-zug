d2be51042d1558c469309a448cda5645ecd652a5
Res <list>
2020-06-11 11:29:06 +0200

    Removed bugs from coronakennzahlen

--- js/stat_zg_coronakennzahlen.js
+++ js/stat_zg_coronakennzahlen.js
@@ -11,12 +11,12 @@ define(['libs/d3_v4'],  function(d3v4) {
 
 	return {
 		load: function() {
-			csv_path ="https://raw.githubusercontent.com/statzg/glibraries-stat-zug/master/daten/result-themen-14-03-01.csv";
+			csv_path ="/behoerden/gesundheitsdirektion/statistikfachstelle/daten/themen/result-themen-14-03-01-e1.csv";
 			d3v4.csv(csv_path, function (data) {
-				format=d3.time.format("%d.%m.%Y")
+				format=d3v4.timeParse("%d.%m.%Y")
 				data.forEach(function(x) {
 					x["Datumstr"] = x["Datum"];
-					x["Datum"] = format.parse(x["Datum"]);
+					x["Datum"] = format(x["Datum"]);
 					x["Anzahl"] = +x["Anzahl"];
 				});
 				x = data.map(function(d) {
@@ -24,12 +24,12 @@ define(['libs/d3_v4'],  function(d3v4) {
 					"Tag": d.Datum
 					};
 				});
-				latestdate = d3.max(x.map(d=>d.Tag));
+				latestdate = d3v4.max(x.map(d=>d.Tag));
 				aktuelleDaten=data.filter(function(d) { return d.Datum >= latestdate })
 				Fallzahl=aktuelleDaten.filter(function(d) { return d.Typ == "Fallzahl" })[0].Anzahl;
 				Genesene=aktuelleDaten.filter(function(d) { return d.Typ == "Genesene" })[0].Anzahl;
 				Verstorbene=aktuelleDaten.filter(function(d) { return d.Typ == "Todesfälle" })[0].Anzahl;
-				dateNameFormat = d3.time.format("%-d.%-m.%Y");
+				dateNameFormat = d3v4.timeFormat("%-d.%-m.%Y");
 				$("#InfiziertePersonen").html("Infizierte Personen: "+Fallzahl);
 				$("#GenesenePersonen").html("Genesene Personen: "+Genesene);
 				$("#VerstorbenePersonen").html("Verstorbene Personen: "+Verstorbene);
