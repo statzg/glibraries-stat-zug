d9a5d06a511f67edb66a3811a7e32c828c209b8a
Res <list>
2020-12-10 21:05:44 +0100

    Updated Corona Interface

--- include examples/corona-interface-2.js
+++ include examples/corona-interface-2.js
@@ -1,5 +1,5 @@
-<div id="default4">
-	<!--<div><p>Altersgruppe auswählen:</p>
+<div id="default2" style="border: 1px solid #e6e6e2; padding:5px 5px 5px 5px; border-radius: 3px; -webkit-border-radius: 3px; -moz-border-radius: 3px;">
+	<!--<div style="background-color: #d0eef7; padding:0px 5px 0px 5px; border-radius: 3px; -webkit-border-radius: 3px; -moz-border-radius: 3px; margin-bottom: 0.5em;"><p style="margin-bottom: 0.3em">Altersgruppe auswählen:</p>
 		<ul class="subelements-listing">
 			<li><a id="0-4" class="alter" href="javascript:;">0-4</a> </li>
 			<li><a id="5-9" class="alter" href="javascript:;">5-9</a> </li>
@@ -23,24 +23,24 @@
 			<li><a id="Alle" class="alter" href="javascript:;">Alle</a> </li>
 		</ul>
 	</div>-->
-	<div><p>Altersgruppe auswählen:</p>
+	<div style="background-color: #d0eef7; padding:0px 5px 0px 5px; border-radius: 3px; -webkit-border-radius: 3px; -moz-border-radius: 3px; margin-bottom: 0.5em;"><p style="margin-bottom: 0.3em">Altersgruppe auswählen:</p>
 		<ul class="subelements-listing">
-			<li><a id="0-24" class="alter selected" href="javascript:;">0-24</a> </li>
-			<li><a id="25-44" class="alter selected" href="javascript:;">25-44</a> </li>
-			<li><a id="45-64" class="alter selected" href="javascript:;">45-64</a> </li>
-			<li><a id="65+" class="alter selected" href="javascript:;">65+</a> </li>
+			<li><a id="0-24" class="alter" href="javascript:;">0-24</a> </li>
+			<li><a id="25-44" class="alter" href="javascript:;">25-44</a> </li>
+			<li><a id="45-64" class="alter" href="javascript:;">45-64</a> </li>
+			<li><a id="65+" class="alter" href="javascript:;">65+</a> </li>
 			<li><a id="Alle" class="alter" href="javascript:;">Alle</a> </li>
 		</ul>
 	</div>
-	<div><p>Geschlecht auswählen:</p>
+	<div style="background-color: #d0eef7; padding:0px 5px 0px 5px; border-radius: 3px; -webkit-border-radius: 3px; -moz-border-radius: 3px; margin-bottom: 0.5em;"><p style="margin-bottom: 0.3em">Geschlecht auswählen:</p>
 		<ul class="subelements-listing">
-			<li><a id="Total" class="geschlecht selected" href="javascript:;">Total</a> </li>
-			<li><a id="männlich" class="geschlecht selected" href="javascript:;">männlich</a> </li>
-			<li><a id="weiblich" class="geschlecht selected" href="javascript:;">weiblich</a> </li>
+			<li><a id="Total" class="geschlecht" href="javascript:;">Total</a> </li>
+			<li><a id="männlich" class="geschlecht" href="javascript:;">männlich</a> </li>
+			<li><a id="weiblich" class="geschlecht" href="javascript:;">weiblich</a> </li>
 			<li><a id="Alle" class="geschlecht" href="javascript:;">Alle</a> </li>
 		</ul>
 	</div>
-	<div><p>Kennzahl von:</p>
+	<div style="background-color: #d0eef7; padding:0px 5px 0px 5px; border-radius: 3px; -webkit-border-radius: 3px; -moz-border-radius: 3px; margin-bottom: 0.5em;"><p style="margin-bottom: 0.3em">Kennzahl von:</p>
 		<ul class="subelements-listing">
 			<li><a id="k1" class="kennzahl" href="javascript:;">Fallzahl</a> </li>
 			<li><a id="k2" class="kennzahl" href="javascript:;">Todesfälle</a> </li>
@@ -50,7 +50,7 @@
 	</div>
 	<div id="title" class="title"></div>
 	<div id="subtitle" class="subtitle"></div>	
-	<div id="chart4"></div>
+	<div id="chart2"></div>
 	<div id="description" class="description"></div>
 	<div id="source" class="source"></div>
 </div>
@@ -60,19 +60,32 @@ require.config({
 	baseUrl: '/behoerden/gesundheitsdirektion/statistikfachstelle/daten/js/'
 });
 
+groupfilter=["0-24","25-44","45-64","65+"]
+characteristics=["Total","männlich","weiblich"]
+
+var dict02 = {
+  "Total": "total",
+  "total": "Total",
+  "männlich": "maennlich",
+  "maennlich": "männlich",
+  "weiblich": "weiblich",
+  "Alle": "alle",
+  "alle":"Alle"
+};
+
 require(['stat_zg_groupedbar'], function (groupedbar) {
-	args4 ={
-		number:4,													
+	args2 ={
+		number:2,													
 		csv_path:"/behoerden/gesundheitsdirektion/statistikfachstelle/daten/themen/result-themen-14-03-04-k1.csv",	
 		dimension:"Geschlecht",											
 		//group:"Anzahl",	
 		stack:"Alterskategorie",
-		characteristics:["Total","männlich","weiblich"],	
+		characteristics:[],	
 		characteristicsStack:["0-24","25-44","45-64","65+"],
 		scale:1,													
 		showTotal:true,												
 		showAnteil:false,
-		groupFilter:["0-24","25-44","45-64","65+"]	
+		groupFilter:[]	
 	}
 
 	if (typeof uri.search(true)["kennzahl"] == "undefined") {
@@ -80,38 +93,55 @@ require(['stat_zg_groupedbar'], function (groupedbar) {
 		window.history.pushState("", "", uri.href());
 	}
 	if (typeof uri.search(true)["kennzahl"] != "undefined") {
-		args4.csv_path="/behoerden/gesundheitsdirektion/statistikfachstelle/daten/themen/result-themen-14-03-04-"+uri.search(true)["kennzahl"]+".csv";
+		args2.csv_path="/behoerden/gesundheitsdirektion/statistikfachstelle/daten/themen/result-themen-14-03-04-"+uri.search(true)["kennzahl"]+".csv";
 		$("#"+uri.search(true)["kennzahl"]).addClass("selected");
 		$("#"+uri.search(true)["kennzahl"]).parent().addClass("selected");
 	}
 	if (typeof uri.search(true)["alter"] != "undefined") {
 		if(typeof uri.search(true)["alter"] =="string") {
-			args4.groupFilter.push(uri.search(true)["alter"]);
+			args2.groupFilter.push(uri.search(true)["alter"]);
 			$("#"+uri.search(true)["alter"]).addClass("selected")
 			$("#"+uri.search(true)["alter"]).parent().addClass("selected")
 		} else {
 			for(k=0; k < uri.search(true)["alter"].length; k++) {
-				args4.groupFilter.push(uri.search(true)["alter"][k]);
+				args2.groupFilter.push(uri.search(true)["alter"][k]);
 				$("#"+uri.search(true)["alter"][k]).addClass("selected")
 				$("#"+uri.search(true)["alter"][k]).parent().addClass("selected")
 			}
 		}
-	}
-	if (typeof uri.search(true)["geschlecht"] != "undefined") {
-		if(typeof uri.search(true)["geschlecht"] =="string") {
-			args4.characteristics.push(uri.search(true)["geschlecht"]);
-			$("#"+uri.search(true)["geschlecht"]).addClass("selected")
-			$("#"+uri.search(true)["geschlecht"]).parent().addClass("selected")
-		} else {
-			for(k=0; k < uri.search(true)["geschlecht"].length; k++) {
-				args4.characteristics.push(uri.search(true)["geschlecht"][k]);
-				$("#"+uri.search(true)["geschlecht"][k]).addClass("selected")
-				$("#"+uri.search(true)["geschlecht"][k]).parent().addClass("selected")
-			}
-		}
+	} else {
+		for(k=0; k < groupfilter.length; k++) {
+			args2.groupFilter.push(groupfilter[k]);
+			uri.addSearch('alter',groupfilter[k]);
+			$("[id='"+groupfilter[k]+"']").addClass("selected");
+			$("[id='"+groupfilter[k]+"']").parent().addClass("selected");
+		};
+		window.history.pushState("", "", uri.href());
 	}
 
-	groupedbar.load(args4);
+	if (typeof uri.search(true)["geschlecht"] != "undefined") {
+		if(typeof uri.search(true)["geschlecht"] =="string") {
+			args2.characteristics.push(dict02[uri.search(true)["geschlecht"]]);
+			$("#"+dict02[uri.search(true)["geschlecht"]]).addClass("selected")
+			$("#"+dict02[uri.search(true)["geschlecht"]]).parent().addClass("selected")
+		} else {
+			for(k=0; k < uri.search(true)["geschlecht"].length; k++) {
+				args2.characteristics.push(dict02[uri.search(true)["geschlecht"][k]]);
+				$("#"+dict02[uri.search(true)["geschlecht"][k]]).addClass("selected")
+				$("#"+dict02[uri.search(true)["geschlecht"][k]]).parent().addClass("selected")
+			}
+		}
+	} else {
+		for(k=0; k < characteristics.length; k++) {
+			args2.characteristics.push(characteristics[k]);
+			uri.addSearch('geschlecht',dict02[characteristics[k]]);
+			$("[id='"+characteristics[k]+"']").addClass("selected");
+			$("[id='"+characteristics[k]+"']").parent().addClass("selected");
+		};
+		window.history.pushState("", "", uri.href());
+	}
+
+	groupedbar.load(args2);
 
 	//Funktion um bei Knopfklick die Alterskategorien einzublenden oder auszublenden.
 	$(".alter").click(function(){
@@ -120,42 +150,42 @@ require(['stat_zg_groupedbar'], function (groupedbar) {
 			$(".alter").parent().addClass("selected")
 			$(this).removeClass("selected")
 			$(this).parent().removeClass("selected")
-			args4.groupFilter=["0-24","25-44","45-64","65+"]
+			args2.groupFilter=["0-24","25-44","45-64","65+"]
 			uri.removeSearch('alter');
 			window.history.pushState("", "", uri.href());
 		} else {
 			shown=false
-			for (k = 0; k < args4.groupFilter.length; k++) {
-				if (this.id==args4.groupFilter[k]) {
+			for (k = 0; k < args2.groupFilter.length; k++) {
+				if (this.id==args2.groupFilter[k]) {
 					shown=true;
 					j=k
 				}
 			}
 			if (shown==false) {
-				args4.groupFilter.push(this.id);
+				args2.groupFilter.push(this.id);
 				$(this).addClass("selected")
 				$(this).parent().addClass("selected")
 				uri.addSearch('alter',this.id);
 				window.history.pushState("", "", uri.href());
 			} else {
-				args4.groupFilter.splice(j, 1)
+				args2.groupFilter.splice(j, 1)
 				$(this).removeClass("selected")
 				$(this).parent().removeClass("selected")
 				uri.removeSearch('alter',this.id);
 				window.history.pushState("", "", uri.href());
 			}
 		}
-		groupedbar.load(args4);
+		groupedbar.load(args2);
 	});
 
 	//Funktion um bei Knopfklick die Kennzahl einzublenden oder auszublenden.
 	$(".kennzahl").click(function(){
 		$(".kennzahl").removeClass("selected");
 		$(".kennzahl").parent().removeClass("selected");
-		args4.csv_path="/behoerden/gesundheitsdirektion/statistikfachstelle/daten/themen/result-themen-14-03-04-"+this.id+".csv";
+		args2.csv_path="/behoerden/gesundheitsdirektion/statistikfachstelle/daten/themen/result-themen-14-03-04-"+this.id+".csv";
 		$(this).addClass("selected");
 		$(this).parent().addClass("selected");
-		groupedbar.load(args4);
+		groupedbar.load(args2);
 		uri.setSearch('kennzahl',this.id);
 		window.history.pushState("", "", uri.href());
 	});
@@ -167,35 +197,35 @@ require(['stat_zg_groupedbar'], function (groupedbar) {
 			$(".geschlecht").parent().addClass("selected")
 			$(this).removeClass("selected")
 			$(this).parent().removeClass("selected")
-			args4.characteristics=["Total","männlich","weiblich"]
+			args2.characteristics=["Total","männlich","weiblich"]
 			uri.removeSearch('geschlecht');
 			window.history.pushState("", "", uri.href());
 		} else {
 			shown=false
-			if (typeof args4.characteristics == "undefined") {
-				args4.characteristics=[];
+			if (typeof args2.characteristics == "undefined") {
+				args2.characteristics=[];
 			}
-			for (k = 0; k < args4.characteristics.length; k++) {
-				if (this.id==args4.characteristics[k]) {
+			for (k = 0; k < args2.characteristics.length; k++) {
+				if (this.id==args2.characteristics[k]) {
 					shown=true;
 					j=k
 				}
 			}
 			if (shown==false) {
-				args4.characteristics.push(this.id);
+				args2.characteristics.push(this.id);
 				$(this).addClass("selected")
 				$(this).parent().addClass("selected")
-				uri.addSearch('geschlecht',this.id);
+				uri.addSearch('geschlecht',dict02[this.id]);
 				window.history.pushState("", "", uri.href());
 			} else {
-				args4.characteristics.splice(j, 1);
+				args2.characteristics.splice(j, 1);
 				$(this).removeClass("selected")
 				$(this).parent().removeClass("selected")
-				uri.removeSearch('geschlecht',this.id);
+				uri.removeSearch('geschlecht',dict02[this.id]);
 				window.history.pushState("", "", uri.href());  
 			}
 		}
-		groupedbar.load(args4);	
+		groupedbar.load(args2);	
 	});
 });
 </script>
